7e6d31cb6838ee5802579a85feb767b47d98103a
josimarminetefera <josimaminete@gmail.com>
2020-12-29 21:52:51 -0300

    Adicionado body parser e alterando o metodo post

--- app.js
+++ app.js
@@ -3,13 +3,23 @@ const express = require("express");
 //INSTANCIA DO EXPRESS
 const app = express();
 
+//PARA FAZER O REGISTRO DE LOGS DE ACESSO
 const morgan = require("morgan");
+
+//ELE DEFINE O CORPO DAS REQUISIÇÕES DE ENTRADA
+const body_parser = require("body-parser")
 
 //CONSTRUINDO ROTA DE PRODUTOS
 const rota_produtos = require("./routes/produto");
 
 //PARA MONITORAR TODA EXECUÇÃO E DAR UM LOG
 app.use(morgan("dev"));
+
+//FALANDO QUE A ENTRADA DE DADOS VAI ACEITAR APENAS DADOS SIMPLES
+app.use(body_parser.urlencoded({ extended: false }));
+//ACEITA APENAS FORMATO JSON DE ENTRADA NO BODY
+app.use(body_parser.json());
+
 app.use("/produto", rota_produtos);
 
 //SE NENHUMA DAS ROTAS ACIMA FUNCIONAR AI ELE CAI AQUI NESTES DE ERRO
--- routes/produto.js
+++ routes/produto.js
@@ -3,19 +3,28 @@ const express = require("express");
 //ABRINDO A ROTA
 const rota = express.Router();
 
-//ROTA DE GET
+//ROTA DE GET PARA LISTAR
 rota.get("/", (req, res, next) => {
     res.status(200).send({
         mensagem: "Página GET de  produto."
     });
 });
 
+//ROTA DE POST CADASTRAR
 rota.post("/", (req, res, next) => {
+
+    const produto = {
+        nome: req.body.nome,
+        preco: req.body.preco,
+    }
+
     res.status(201).send({
-        mensagem: "Pagina POST de produto."
+        mensagem: "Pagina POST de produto.",
+        produto: produto
     })
 });
 
+//ROTA DE GET PARA VISUALIZAR
 rota.get("/:id_produto", (req, res, next) => {
     const id = req.params.id_produto
     if (id == 123) {
@@ -31,12 +40,14 @@ rota.get("/:id_produto", (req, res, next) => {
     }
 });
 
+//ROTA DE PATCH
 rota.patch("/", (req, res, next) => {
     res.status(201).send({
         mensagem: "Pagina PATCH de produto."
     })
 });
 
+//PARA REMOÇÃO DE DADOS 
 rota.delete("/", (req, res, next) => {
     res.status(201).send({
         mensagem: "Pagina DELETE de produto."
