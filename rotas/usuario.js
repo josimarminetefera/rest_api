771f5044359ef3da0db00f28e80a12922b35191e
josimarminetefera <josimaminete@gmail.com>
2021-01-21 19:24:42 -0300

    Adicionado validação de usuário ja cadastrado

--- rotas/usuario.js
+++ rotas/usuario.js
@@ -17,35 +17,51 @@ rota.post("/cadastrar", (req, res, next) => {
         if (erro) {
             return res.status(500).send({ erro: erro, response: null });
         }
-        //o 10 é o tamanho da complexidade da senha 
-        bcrypt.hash(req.body.senha, 10, (erro, hash) => {
-            console.log("usuario.js - INICIANDO FUNÇÃO bcrypt "+erro);
-            if (erro) {
-                return res.status(500).send({ erro: erro, response: null });
-            }
-            //abrir a conecção sql para inserir os dados no banco 
-            conexao.query(
-                "INSERT INTO usuario (email, senha) values(?, ?)",
-                [req.body.email, hash],
-                (erro, resultado) => {
-                    console.log("usuario.js - INICIANDO FUNÇÃO conexao.query");
-                    //fechar conexão para naão manter ela aberta para as outras coisas
-                    conexao.release();
-                    if (erro) {
-                        return res.status(500).send({ erro: erro, response: null });
-                    }
 
-                    const resposta = {
-                        mensagem: "Usuario criado com sucesso",
-                        usuario: {
-                            id: resultado.insertId,
-                            email: req.body.email
-                        }
-                    }
-                    return res.status(201).send({ resposta })
+        //verificar se ja tem email cadastrado
+        conexao.query(
+            "SELECT * FROM usuario WHERE email = ?",
+            [req.body.email],
+            (erro, resultado) => {
+                if (erro) {
+                    return res.status(500).send({ erro: erro, response: null });
                 }
-            );
-        });
+
+                if (resultado.length > 0) {
+                    return res.status(401).send({ erro: "Usuario já cadastrado", response: null });
+                } else {
+                    //o 10 é o tamanho da complexidade da senha 
+                    bcrypt.hash(req.body.senha, 10, (erro, hash) => {
+                        console.log("usuario.js - INICIANDO FUNÇÃO bcrypt " + erro);
+                        if (erro) {
+                            return res.status(500).send({ erro: erro, response: null });
+                        }
+                        //abrir a conecção sql para inserir os dados no banco 
+                        conexao.query(
+                            "INSERT INTO usuario (email, senha) values(?, ?)",
+                            [req.body.email, hash],
+                            (erro, resultado) => {
+                                console.log("usuario.js - INICIANDO FUNÇÃO conexao.query");
+                                //fechar conexão para naão manter ela aberta para as outras coisas
+                                conexao.release();
+                                if (erro) {
+                                    return res.status(500).send({ erro: erro, response: null });
+                                }
+
+                                const resposta = {
+                                    mensagem: "Usuario criado com sucesso",
+                                    usuario: {
+                                        id: resultado.insertId,
+                                        email: req.body.email
+                                    }
+                                }
+                                return res.status(201).send({ resposta })
+                            }
+                        );
+                    });
+                }
+            }
+        );
     });
 });
 
